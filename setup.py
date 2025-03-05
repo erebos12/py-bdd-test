d9a158c19a141f1ce5791e293e501c04f726101c
Alex Sahm <alexander.sahm@gmx.de>
2025-03-05 14:30:56 +0100

    use PyHamcrest intead of hamcrest

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         "kafka-python",
         "behave",
-        "hamcrest",
+        "PyHamcrest",
     ],
     author="Your Name",
     author_email="your.email@example.com",
