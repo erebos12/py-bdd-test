67d907744e5397c8a01f49f7907f3a5fa884089c
Alex Sahm <alexander.sahm@gmx.de>
2025-03-05 16:14:58 +0100

    fixed build od py-bdd-test

--- features/steps/config.py
+++
@@ -1,4 +0,0 @@
-EATER_CSV_UPLOAD_URL = "http://eater:8080/csv"
-DICE_CSV_UPLOAD_URL = "http://dice:8080/upload"
-DELIMITER = ";"
-COOPER_URL = "cooper:6565"
--- setup.py
+++ setup.py
@@ -2,24 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-bdd-test",
-    version="1.0.5",
-    packages=find_packages(include=["bdd-test", "bdd-test.*"]),
-    package_data={"bdd-test": ["features/**/*", "behave.ini"]},
+    version="1.0.6",
+    packages=find_packages(include=["py_bdd_test", "py_bdd_test.*"]),
+    package_data={
+        "py_bdd_test": ["features/steps/*.py"],
+    },
+    include_package_data=True,
     install_requires=[
-        "kafka-python",
         "behave",
-        "PyHamcrest",
+        "parse",
+        "parse_type",
+        "kafka-python",
+        "pyhamcrest",
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A Python package for BDD testing with Kafka integration.",
-    long_description=open("readme.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/erebos12/py-bdd-test",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
 )
