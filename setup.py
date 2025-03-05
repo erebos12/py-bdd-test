8bfc43fa62f57d71a2973761215d791895e30ecf
Alex Sahm <alexander.sahm@gmx.de>
2025-03-05 14:21:00 +0100

    v1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-bdd-test",
-    version="0.1.0",
+    version="1.0.3",
     packages=find_packages(include=["bdd-test", "bdd-test.*"]),
     package_data={"bdd-test": ["features/**/*", "behave.ini"]},
     install_requires=[
