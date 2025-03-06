a19fdc0fd8ed83ebcac9801a000149518fcb2972
Alex Sahm <alexander.sahm@adesso.de>
2025-03-06 13:30:38 +0100

    Bump version to 1.0.24

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-bdd-test",
-    version='1.0.23',
+    version='1.0.24',
     packages=find_packages(exclude=["bdd-test", "bdd-test.*"]),
     install_requires=[
         "behave",
