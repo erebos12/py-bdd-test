ebedbae36c93c0fa0cbbe4374f56bc8800f79110
Alex Sahm <alexander.sahm@gmx.de>
2025-03-06 09:34:29 +0100

    use underscore for package name

--- setup.py
+++ setup.py
@@ -2,17 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-bdd-test",
-    version="1.0.12",
-    packages=find_packages(include=["py_bdd_test", "py_bdd_test.*"]),  # Ensure submodules are found
-    include_package_data=True,  # Needed for non-Python files
-    package_data={
-        "py_bdd_test": ["features/steps/*.py", "features/*.feature"],
-    },
+    version="1.0.13",
+    packages=find_packages(),  # Automatically finds "py_bdd_test"
     install_requires=[
         "behave",
+        "kafka-python",
         "parse",
         "parse_type",
-        "kafka-python",
-        "pyhamcrest",
+        "pyhamcrest"
     ],
+    python_requires=">=3.7",
 )
