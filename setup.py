c085d69694fd9546472fd2fc0cc9f4bf064ecc27
Alex Sahm <alexander.sahm@gmx.de>
2025-03-06 09:25:23 +0100

    v1.0.12

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-bdd-test",
-    version="1.0.11",
-    packages=find_packages(include=["py_bdd_test", "py_bdd_test.*"]),
+    version="1.0.12",
+    packages=find_packages(include=["py_bdd_test", "py_bdd_test.*"]),  # Ensure submodules are found
+    include_package_data=True,  # Needed for non-Python files
     package_data={
         "py_bdd_test": ["features/steps/*.py", "features/*.feature"],
     },
-    include_package_data=True,
     install_requires=[
         "behave",
         "parse",
